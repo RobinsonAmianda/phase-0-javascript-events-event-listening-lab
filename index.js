46807820bbb927493c2238b97e40b3e6da5d1000
Robinson Amianda <robinsonamianda5@gmail.com>
2023-09-29 06:50:14 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+ document.getElementById(`#input`)
+addEventListener(`click`,clickAlert)
+function clickAlert(event){
+    console.log("I was clicked!")
+} 
+function addingEventListener(){
+    let main =document.getElementById(`main`)
 }
+main.addEventListeners(`click`,addingEventListener)
+    console.log(`true`)
